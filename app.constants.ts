95c9fd4a516a68da16594fec35131c6325147bac
Viktor Petruk <damnnn261@gmail.com>
2024-11-18 20:32:38 +0200

    git add .

---
+++ app.constants.ts
@@ -0,0 +1,7 @@
+export const APP_WRITE_ID = 'nuxt-database'
+export const DB_ID = 'blog-base'
+export const COLLECTION_BLOGS = 'blogs'
+export const COLLECTION_CREATORS = 'creators'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
+
