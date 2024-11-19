ee8f00dc615eca1911c89411aae23e0fa0e38f5c
Viktor Petruk <damnnn261@gmail.com>
2024-11-19 13:10:50 +0200

    add input component from shadcn

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import Login from './login.vue';
+
 
 </script>
 <template>
 	<div class="">
-
+		<Login />
 	</div>
-	
+
 </template>
 
 
--- pages/login.vue
+++ pages/login.vue
@@ -3,8 +3,14 @@
 </script>
 
 <template>
-	<div>
+	<div class="flex items-center justify-center min-h-screen w-full ">
+		<div class="rounded bg-sidebarBg w-1/4 p-5 ">
+			<h1 class="text-2xl text-white hover:text-colorSidebar font-bold text-center mb-5">Login</h1>
 
+			<form>
+				
+			</form>
+		</div>
 	</div>
 </template>
 
