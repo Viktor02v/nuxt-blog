56da0a2429a71020e1c3aa7dfbcd0cb87836dab6
Viktor Petruk <damnnn261@gmail.com>
2024-11-21 10:13:19 +0200

    add textarea component from shadcn

--- components/layout/user/createBlog.vue
+++ components/layout/user/createBlog.vue
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/vue-query";
 import { v4 as uuid } from 'uuid';
 import { COLLECTION_BLOGS, DB_ID } from "@/app.constants";
 import type { BlogCard } from "../../blogger/blogger.types";
+import { DB } from "~/lib/appwrite";
 
 const isOpenForm = ref<boolean>(false)
 
@@ -13,6 +14,28 @@ const props = defineProps({
 		type: Function,
 	}
 })
+
+const { handleSubmit, defineField, handleReset } = useForm<BlogForm>()
+
+const [title, titleAttrs] = defineField('title')
+const [about, aboutAttrs] = defineField('about')
+const [foto1_url, foto1_urlAttrs] = defineField('foto1_url')
+const [description1, description1Attrs] = defineField('description1')
+const [foto2_url, foto2_urlAttrs] = defineField('foto2_url')
+const [description2, description2Attrs] = defineField('description2')
+
+const { mutate, isPending } = useMutation({
+	mutationKey: ['create a new blog'],
+	mutationFn: (data: BlogForm) => DB.createDocument(DB_ID, COLLECTION_BLOGS, uuid(), data),
+	onSuccess() {
+		props.refetch && props.refetch()
+		handleReset()
+	}
+})
+
+const onSubmit = handleSubmit(values =>{
+	mutate(values)
+})
 </script>
 
 <template>
@@ -22,11 +45,10 @@ const props = defineProps({
 			<Icon v-if="!isOpenForm" class="text-title" name="icons8:plus" size="70" />
 		</button>
 
-		<form>
-
+		<form v-if="isOpenForm" @submit="onSubmit" class="form">
+			<UiInput />
 		</form>
 	</section>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
