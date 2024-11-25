565b71e2d09bac2aaf3157be291b72122a1e224d
Viktor Petruk <damnnn261@gmail.com>
2024-11-25 18:27:39 +0200

    create function formatTime in formatDate.ts, some fixations in comment styles

--- components/blogger/formatDate.ts
+++ components/blogger/formatDate.ts
@@ -2,3 +2,7 @@ import dayjs from 'dayjs';
 export function formatDate(date: string) {
 	return dayjs(date).format('DD.MM.YYYY');
 }
+
+export function formatTime(date: string) {
+	return dayjs(date).format('hh:mm A');
+}
--- components/layout/blog/Comment.vue
+++ components/layout/blog/Comment.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { formatDate } from '@/components/blogger/formatDate'
+import { formatDate, formatTime } from '@/components/blogger/formatDate'
+
+
+const authStore = useAuthStore()
 const props = defineProps({
 	comments: Array,
 	default: ''
@@ -8,13 +11,27 @@ const props = defineProps({
 
 <template>
 	<ul class="">
-		<li v-for="comment in props.comments || []" :key="comment.$id"
-			class="mb-2 rounded flex p-2 gap-3 border border-2 border-sidebarBg hover:border-colorSidebar">
-			<Icon name="ic:outline-comment" class="text-colorSidebar" />
+		<li v-for="comment in props.comments || []" :key="comment.$id" class="mb-10">
+			<div class="flex mb-1">
+				<div class="flex items-center gap-1 p-1 rounded text-colorSidebar border border-sidebarBg bg-white">
+					<span class="text-[1.2rem] font-light">
+						by:
+					</span>
+					<p class="text-[1.3rem]">{{ authStore.user.name }}</p>
+				</div>
+			</div>
+			<div class="mb-2 rounded flex p-2 justify-around border border-2 border-sidebarBg hover:border-colorSidebar">
+				<Icon name="ic:outline-comment" class="text-colorSidebar" />
 
-			<p class="w-[840px] px-3 text-white break-words rounded bg-sidebarBg whitespace-normal">{{ comment.text }}</p>
+				<p class="w-[840px] px-3 text-white break-words rounded bg-sidebarBg whitespace-normal">{{ comment.text }}
+				</p>
 
-			<p class="self-end text-white rounded p-1 bg-sidebarBg text-[1.4rem]">{{ formatDate(comment.$createdAt) }}</p>
+				<div class="self-end text-white flex flex-col items-center  rounded p-1 bg-sidebarBg">
+					<span class="font-light  text-[1.1rem]">{{ formatTime(comment.$createdAt) }}</span>
+					<p class=" text-[1.3rem]">{{ formatDate(comment.$createdAt) }}
+					</p>
+				</div>
+			</div>
 		</li>
 	</ul>
 </template>
