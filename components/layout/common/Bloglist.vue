e4056dfd0961480e1fcce899c4b58817e073fe34
Viktor Petruk <damnnn261@gmail.com>
2024-11-27 09:31:20 +0200

    craete a window route to the create blog page, in case there  are no blogs available

--- components/layout/common/Bloglist.vue
+++ components/layout/common/Bloglist.vue
@@ -19,6 +19,18 @@ const { data: blogs, isLoading, error } = useBloggerQuery()
 				<LayoutCommonBlogcard :blog="blog" />
 			</div>
 		</div>
+
+		<div v-if="blogs?.length === 0" class="w-full h-full flex items-center justify-center">
+			<div class="w-[600px] flex justify-center animation bg-sidebarBg rounded p-10">
+				<div class="flex justify-center">
+					<div class="bg-white flex flex-col text-colorSidebar rounded p-5">
+						<p class="border-b pb-2 border-colorSidebar">There are no blogs available now.</p>
+						<p>Go on and create one right <NuxtLink to="/user/create-blog" class="underline">now</NuxtLink></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</section>
 </template>
 
