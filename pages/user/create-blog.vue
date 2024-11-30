120da84f240c074d5c30dc685dbf053691dd8f0b
Viktor Petruk <damnnn261@gmail.com>
2024-11-30 15:08:46 +0200

    add mobile adaptivity for create blog view

--- pages/user/create-blog.vue
+++ pages/user/create-blog.vue
@@ -4,12 +4,12 @@
 
 <template>
 	<main class="p-10 w-full h-full">
-		<h1 class="font-bold  text-center text-title text-4xl mb-10">
+		<h1 class="font-bold  text-center text-title text-3xl mb-10">
 			Create Blog | Blogger
 		</h1>
 
 		<div class="w-full flex items-center justify-center">
-		<LayoutUserCreateBlog class="flex w-1/2 items-center justify-center"  />
+		<LayoutUserCreateBlog class="flex md:w-1/2 items-center justify-center"  />
 	</div>
 	</main>
 </template>
